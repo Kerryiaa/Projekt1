7db45b08d9a1da5cf75e3d5bc5bfe4ccced9b321
Kerryiaa <kinga.mazurek@poczta.onet.pl>
2022-05-22 12:13:02 +0200

    ElementsGame

--- js/app.js
+++ js/app.js
@@ -1,83 +1,41 @@
-
-
-repeatElements = [];
-function checkElement(event) {
- 
-  if (event.keyCode == 13){
-   // console.log(event.target.value);
-   if(pierwiastki[losuj][1]== event.target.value){
-  //console.log("dobrze");
-  pierwiastek[losuj].parentElement.classList.remove("checked");
-  pierwiastek[losuj].parentElement.classList.add("goodAnswer");
-  
-
-   }
-else{
- // console.log("źle");
-  pierwiastek[losuj].parentElement.classList.remove("checked");
-  pierwiastek[losuj].parentElement.classList.add("badAnswer");
-
-  
-  }
-  game();
-}
-
- 
-}
-
-
+var repeatElements = [];
 container = document.getElementsByClassName("symbol");
-  // console.log(container);
-  pierwiastek = [];
-  y = 0;
-  for (x = 0; x < container.length; x++ ){
-     // console.log(container[x].innerHTML);
-      if (container[x].textContent != 'DE' && container[x].textContent != 'DEL' && container[x].textContent != '57-71'  && container[x].textContent != '89-103' ) {
-// console.log(container[x]);
-          pierwiastek[y] = container[x];
-          
-          console.log(y+"-"+pierwiastek[y].textContent);
-          y++;
-      }
-  }
-function game(){
-  
-if(repeatElements.length >= pierwiastki.length){
-    
-} else{
-console.log(pierwiastki.length)
-console.log(repeatElements.length)
-  losuj = Math.floor(Math.random() * pierwiastki.length);
-    // console.log(pierwiastki[losuj]);
-  
-    if(repeatElements.includes(losuj)){
+pierwiastek = [];
+y = 0;
+for (x = 0; x < container.length; x++) {
+    if (container[x].textContent != 'DE' && container[x].textContent != 'DEL' && container[x].textContent != '57-71' && container[x].textContent != '89-103') {
+        pierwiastek[y] = container[x];
 
-    //  console.log("powtorzylem:"+pierwiastek[losuj].textContent);
-      game();
-    } else {
- 
-  for(x = 0; x < pierwiastek.length; x++){
-    //iteruje x dopóki nie jest zgodny z numerem pierwiastka
-    // console.log(container[x].textContent)
-    //wyświetla w konsoli symbol pierwiastka (np.Na) 
-    // console.log(pierwiastek[losuj])
-    // wyświetla wylosowany pieriwastek w konsoli
-
-// console.log("nazwa:"+pierwiastek[x].textContent);
-// console.log("wylosowany:"+pierwiastki[losuj][0]);
-
-    if (pierwiastek[x].textContent == pierwiastki[losuj][0]){
-      //sprawdza czy wylosowany x jest zgodny z numerem pierwiastka
-            // console.log(pierwiastek[losuj]);
-            pierwiastek[x].parentElement.classList.add("checked");
-          //podświetla wylosowany pieriwastek, dodaje go do listy klas 
+        console.log(y + "-" + pierwiastek[y].textContent);
+        y++;
     }
-
-  }
-  repeatElements.push(losuj)
-}
-
-}
 }
 game();
 
+function game() {
+    console.log(repeatElements);
+    if (repeatElements.length >= pierwiastki.length) {
+        console.log("koniec gry");
+    } else {
+        losuj = Math.floor(Math.random() * pierwiastki.length);
+        if (repeatElements.includes(losuj)) {
+            game();
+        } else {
+            pierwiastek[losuj].parentElement.classList.add("checked");
+            repeatElements.push(losuj)
+        }
+    }
+}
+
+function checkElement(event) {
+    if (event.keyCode == 13) {
+        if (event.target.value == pierwiastki[losuj][1]) {
+            pierwiastek[losuj].parentElement.classList.remove("checked");
+            pierwiastek[losuj].parentElement.classList.add("goodAnswer");
+        } else {
+            pierwiastek[losuj].parentElement.classList.remove("checked");
+            pierwiastek[losuj].parentElement.classList.add("badAnswer");
+        }
+        game();
+    }
+}
