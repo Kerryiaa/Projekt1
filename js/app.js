63279addd1c2bff86fe53744f623ba3df05f78a6
Kerryiaa <kinga.mazurek@poczta.onet.pl>
2022-05-28 13:18:13 +0200

    game with timer

--- js/app.js
+++ js/app.js
@@ -15,6 +15,8 @@ game();
 function game() {
     console.log(repeatElements);
     if (repeatElements.length >= elements.length) {
+        clearInterval(x);
+        timer.innerHTML = "Koniec gry!!!";
         console.log("koniec gry");
     } else {
         randomElement = Math.floor(Math.random() * elements.length);
@@ -32,10 +34,45 @@ function checkElement(event) {
         if (event.target.value == elements[randomElement][1]) {
             elementsSymbol[randomElement].parentElement.classList.remove("checked");
             elementsSymbol[randomElement].parentElement.classList.add("goodAnswer");
+            countDownDate = new Date().getTime() + ( 0.18 * 60 * 1000 );
+            
         } else {
             elementsSymbol[randomElement].parentElement.classList.remove("checked");
             elementsSymbol[randomElement].parentElement.classList.add("badAnswer");
+            countDownDate = new Date().getTime() + ( 0.18 * 60 * 1000 );
         }
+        event.target.value = "";
         game();
     }
+}
+
+
+// Set the date we're counting down to
+var countDownDate = new Date().getTime() + ( 0.18 * 60 * 1000 );
+// Update the count down every 1 second
+var x = setInterval (time,1000);
+function time() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+ 
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    //  clearInterval(x);
+    document.getElementById("timer").innerHTML = "Czas minął";
+    countDownDate = new Date().getTime() + (10 * 1000);
+    elementsSymbol[randomElement].parentElement.classList.add("badAnswer");
+    game();
+}
 }
